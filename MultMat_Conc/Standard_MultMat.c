f2532c90712f4206edc102ce8ff58f153ff5f514
Aniol0012 <aniol0012@gmail.com>
2023-12-18 17:36:33 +0100

    fix: removed useless code

--- MultMat_Conc/Standard_MultMat.c
+++ MultMat_Conc/Standard_MultMat.c
@@ -104,63 +104,6 @@ float **standardMultiplication_ijk(float **matrixA, float **matrixB, int n) {
 
 
 /*
- * Standard ijk Matrix multiplication with O(n^3) time complexity. (sequential)
-*/
-float **standardMultiplication_ijk_sec(float **matrixA, float **matrixB, int n) {
-    struct timespec start, finish;
-    float **result;
-    int i, j, k;
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-
-    result = (float **) malloc(n * sizeof(float *));
-    for (i = 0; i < n; i++) {
-        result[i] = (float *) malloc(n * sizeof(float));
-        memset(result[i], 0, n * sizeof(float));
-        for (j = 0; j < n; j++) {
-            for (k = 0; k < n; k++) {
-                result[i][j] = result[i][j] + (matrixA[i][k] * matrixB[k][j]);
-            }
-        }
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &finish);
-    elapsed_std = (finish.tv_sec - start.tv_sec);
-    elapsed_std += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-
-    return result;
-}
-
-
-/*
- * Standard ikj Matrix multiplication with O(n^3) time complexity.
-*/
-float **standardMultiplication_ikj_sec(float **matrixA, float **matrixB, int n) {
-    struct timespec start, finish;
-    float **result;
-    int i, j, k;
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-
-    result = (float **) malloc(n * sizeof(float *));
-    for (i = 0; i < n; i++) {
-        result[i] = (float *) malloc(n * sizeof(float));
-        memset(result[i], 0, n * sizeof(float));
-        for (k = 0; k < n; k++) {
-            for (j = 0; j < n; j++) {
-                result[i][j] = result[i][j] + (matrixA[i][k] * matrixB[k][j]);
-            }
-        }
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &finish);
-    elapsed_std = (finish.tv_sec - start.tv_sec);
-    elapsed_std += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-
-    return result;
-}
-
-/*
  * Auxiliary function to process a section of the matrix multiplication.
 */
 void *process_section(section_data *data) {
